0ad3870b4319643b0b2b7e3eabfa13edfad36014
MariaVasilieva <mdvasilieva@gmail.com>
2024-06-28 16:11:34 +0300

    First commit

---
+++ src/main/java/org/example/DatabaseInitService.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseInitService {
+    public static void main(String[] args) {
+        String path = "sql\\init_db.sql";
+        try (Connection connection = Database.getInstance().getConnection();
+             Statement statement = connection.createStatement()){
+            String sqlQuery = readSQLFile(path);
+            System.out.println(sqlQuery);
+            statement.executeUpdate(sqlQuery);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String readSQLFile(String path) {
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            br.lines().forEach(line -> sb.append(line).append("\n"));
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return sb.toString();
+    }
+}
